e86ba401d23b6564943e47cb49fca0592738acab
johnborges <1888122+johnborges@users.noreply.github.com>
2024-04-03 13:39:54 -0400

    working setup

--- samples/bot-people-picker-adaptive-card/csharp/PeoplePicker/AdapterWithErrorHandler.cs
+++ samples/bot-people-picker-adaptive-card/csharp/PeoplePicker/AdapterWithErrorHandler.cs
@@ -27,7 +27,7 @@ namespace PeoplePicker
                 logger.LogError(exception, $"[OnTurnError] unhandled error : {exception.Message}");
 
                 // Uncomment below commented line for local debugging..
-                // await turnContext.SendActivityAsync($"Sorry, it looks like something went wrong. Exception Caught: {exception.Message}");
+                await turnContext.SendActivityAsync($"Sorry, it looks like something went wrong. Exception Caught: {exception.Message}");
 
                 // Send a trace activity, which will be displayed in the Bot Framework Emulator
                 await SendTraceActivityAsync(turnContext, exception);
